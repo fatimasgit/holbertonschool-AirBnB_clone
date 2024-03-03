7d9f11e1639bb78df26b26b18bd40d8256f0e685
MrNeonn75 <devneonn75@gmail.com>
2024-03-03 22:10:32 +0400

    Console 0.1 bug fix

--- console.py
+++ console.py
@@ -56,6 +56,27 @@ class HBNBCommand(cmd.Cmd):
         """ Function for quit """
         return True
 
+    def _key_value_parser(self, args):
+        """creates a dictionary from a list of strings"""
+        new_dict = {}
+        for arg in args:
+            if "=" in arg:
+                kvp = arg.split('=', 1)
+                key = kvp[0]
+                value = kvp[1]
+                if value[0] == value[-1] == '"':
+                    value = shlex.split(value)[0].replace('_', ' ')
+                else:
+                    try:
+                        value = int(value)
+                    except:
+                        try:
+                            value = float(value)
+                        except:
+                            continue
+                new_dict[key] = value
+        return new_dict 
+    
     def do_create(self, arg):
         """Creates a new instance of a class"""
         args = arg.split()
