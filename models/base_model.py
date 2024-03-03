a4d118a08754ee7dcf7a5568ec232e5b23fe84fb
MrNeonn75 <devneonn75@gmail.com>
2024-03-03 18:01:43 +0400

    5. Store first object bug fix

--- models/base_model.py
+++ models/base_model.py
@@ -1,9 +1,9 @@
 import uuid  #unik ID generate etmek ucun modul
 from datetime import datetime  # datetime ucun modul
 # Import the variable storage
-from models.engine import __init__
-
-storage = __init__.storage
+from models.engine import __init__ as storage_engine
+# Import models
+import models
 
 class BaseModel:
     def __init__(self, *args, **kwargs):
@@ -41,9 +41,9 @@ class BaseModel:
     def save(self):
         self.updated_at = datetime.now()
         # If it’s a new instance (not from a dictionary representation), add a call to the method new(self) on storage
-        storage.new()
+        storage_engine.storage.new()
         # Save data
-        storage.save()
+        storage_engine.storage.save()
         
     def to_dict(self):
         obj_dict = self.__dict__.copy()
