28de9be7e635f5f8c95f50325490f779dc11e692
MrNeonn75 <devneonn75@gmail.com>
2024-03-03 20:27:34 +0400

    7. Console 0.1

--- console.py
+++ console.py
@@ -5,6 +5,8 @@ This file is for command line with Python
 
 import cmd
 from models.base_model import BaseModel
+from models import engine
+import shlex
 
 # Classes
 classes = {
@@ -15,7 +17,7 @@ classes = {
     # "Review": Review,
     # "State": State,
     # "User": User
-    }
+}
 
 class HBNBCommand(cmd.Cmd):
     """
@@ -60,6 +62,7 @@ class HBNBCommand(cmd.Cmd):
         if len(args) == 0:
             print("** class name missing **")
             return False
+        
         if args[0] in classes:
             new_dict = self._key_value_parser(args[1:])
             instance = classes[args[0]](**new_dict)
@@ -68,6 +71,97 @@ class HBNBCommand(cmd.Cmd):
             return False
         print(instance.id)
         instance.save()
+        
+    def do_show(self, arg):
+        """ Prints the string representation of an instance based on the class name and id """
+        args = arg.split()
+        
+        if (len(args) == 0):
+            print("** class name missing **")
+            return False
+        
+        if (args[0] in classes):
+            if (len(args) > 1):
+                key = args[0] + "." + args[1]
+                if key in engine.storage.all():
+                    print(engine.storage.all()[key])
+                else:
+                    print("** no instance found **")
+            else: print("** instance id missing **")
+        else: print("** class doesn't exist **")
     
-if __name__ == '__main__':
+    def do_destroy(self, arg):
+        """Deletes an instance based on the class and id"""
+        args = shlex.split(arg)
+        if (len(args) == 0):
+            print("** class name missing **")
+        elif (args[0] in classes):
+            if (len(args) > 1):
+                key = args[0] + "." + args[1]
+                if (key in engine.storage.all()):
+                    engine.storage.all().pop(key)
+                    engine.storage.save()
+                else:
+                    print("** no instance found **")
+            else:
+                print("** instance id missing **")
+        else:
+            print("** class doesn't exist **")
+
+    def do_all(self, arg):
+        """Prints string representations of instances"""
+        args = shlex.split(arg)
+        obj_list = []
+        if (len(args) == 0):
+            obj_dict = engine.storage.all()
+        elif (args[0] in classes):
+            obj_dict = engine.storage.all(classes[args[0]])
+        else:
+            print("** class doesn't exist **")
+            return False
+        for key in obj_dict:
+            obj_list.append(str(obj_dict[key]))
+        print("[", end="")
+        print(", ".join(obj_list), end="")
+        print("]")
+
+    def do_update(self, arg):
+        """Update an instance based on the class name, id, attribute & value"""
+        args = shlex.split(arg)
+        integers = ["number_rooms", "number_bathrooms", "max_guest",
+                    "price_by_night"]
+        floats = ["latitude", "longitude"]
+        if (len(args) == 0):
+            print("** class name missing **")
+        elif (args[0] in classes):
+            if (len(args) > 1):
+                k = args[0] + "." + args[1]
+                if (k in engine.storage.all()):
+                    if (len(args) > 2):
+                        if (len(args) > 3):
+                            if (args[0] == "Place"):
+                                if (args[2] in integers):
+                                    try:
+                                        args[3] = int(args[3])
+                                    except:
+                                        args[3] = 0
+                                elif (args[2] in floats):
+                                    try:
+                                        args[3] = float(args[3])
+                                    except:
+                                        args[3] = 0.0
+                            setattr(engine.storage.all()[k], args[2], args[3])
+                            engine.storage.all()[k].save()
+                        else:
+                            print("** value missing **")
+                    else:
+                        print("** attribute name missing **")
+                else:
+                    print("** no instance found **")
+            else:
+                print("** instance id missing **")
+        else:
+            print("** class doesn't exist **")
+            
+if (__name__ == '__main__'):
     HBNBCommand().cmdloop()
