ab474403bfcf3d1b8c59e99d82aa0b0d757a3296
MrNeonn75 <devneonn75@gmail.com>
2024-03-03 18:44:00 +0400

    5. Store first object bug fix

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,9 +1,23 @@
 import json
 from json.decoder import JSONDecodeError
+from models.base_model import BaseModel
 
 """
 Storage file for storing the data
 """
+
+class BaseModelEncoder(json.JSONEncoder):
+    """
+    Class for encode
+    """
+    def default(self, obj):
+        """ Object to dict """
+        if isinstance(obj, BaseModel):
+            # Convert BaseModel instance to dictionary representation
+            return obj.to_dict()
+        # Let the base class default method handle other types
+        return super().default(obj)
+
 
 class FileStorage:
     """
@@ -27,7 +41,7 @@ class FileStorage:
         """ Serializes __objects to the JSON file (path: __file_path) """
         
         with open(self.__file_path, "w") as file:
-            json.dump(self.__objects, file)
+            json.dump(self.__objects, file, cls=BaseModelEncoder)
     
     def reload(self):
 
